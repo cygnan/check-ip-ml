e398f54eadb1b4907d5996553d257abe32fba70d
Cygnan <email@cygnan.com>
2018-02-18 07:30:06 +0900

    - Add a function to serve only an IP address (#8)
    * - Add a function to serve only an IP address
    
    - Add a function to redirect when URL is invalid
    
    * Fix some variables' name

--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
+    "fmt"
     "net/http"
-
     "google.golang.org/appengine"
+    "strings"
     "html/template"
 )
+
+func main() {
+    http.HandleFunc("/", requestHandler)
+    appengine.Main()
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+    if isInvalidURL(r) {
+        http.Redirect(w, r, "https://check-ip.ml", 301)
+    }
+
+    if strings.Contains(r.Host, "raw") {
+        serveRawIP(w, r)
+    } else {
+        serveHTML(w, r)
+    }
+}
+
+func isInvalidURL(r *http.Request) bool {
+    host := getHost(r)
+
+    return !strings.HasSuffix(host, "check-ip.ml") &&
+        !strings.HasSuffix(host, "localhost")
+}
+
+func getHost(r *http.Request) string {
+    host := r.Host
+
+    if i := strings.Index(r.Host, ":"); i != -1 {
+        host = r.Host[:i]
+    }
+
+    return host
+}
+
+func serveRawIP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, r.RemoteAddr)
+}
 
 var (
     indexTemplate = template.Must(template.ParseFiles("index.html"))
@@ -15,12 +54,7 @@ type templateParams struct {
     IP string
 }
 
-func main() {
-    http.HandleFunc("/", indexHandler)
-    appengine.Main()
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func serveHTML(w http.ResponseWriter, r *http.Request) {
     params := templateParams{IP: r.RemoteAddr}
 
     indexTemplate.Execute(w, params)
