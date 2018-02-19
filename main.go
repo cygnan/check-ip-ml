c1cd6119a97f9f94a2979c17ab44c1ddacade0d6
Cygnan <email@cygnan.com>
2018-02-19 15:20:37 +0900

    Use map (#10)
    * Use map
    
    * Drop the else and outdent its block

--- main.go
+++ main.go
@@ -1,61 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "google.golang.org/appengine"
-    "strings"
-    "html/template"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+
+	"google.golang.org/appengine"
 )
 
 func main() {
-    http.HandleFunc("/", requestHandler)
-    appengine.Main()
+	http.HandleFunc("/", requestHandler)
+	appengine.Main()
 }
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
-    if isInvalidURL(r) {
-        http.Redirect(w, r, "https://check-ip.ml", 301)
-    }
+	if isInvalidURL(r) {
+		http.Redirect(w, r, "https://check-ip.ml", 301)
+	}
 
-    if strings.Contains(r.Host, "raw") {
-        serveRawIP(w, r)
-    } else {
-        serveHTML(w, r)
-    }
+	if strings.Contains(r.Host, "raw") {
+		serveRawIP(w, r)
+	} else {
+		serveHTML(w, r)
+	}
 }
 
 func isInvalidURL(r *http.Request) bool {
-    host := getHost(r)
+	host := getHost(r)
 
-    return !strings.HasSuffix(host, "check-ip.ml") &&
-        !strings.HasSuffix(host, "localhost")
+	return !strings.HasSuffix(host, "check-ip.ml") &&
+		!strings.HasSuffix(host, "localhost")
 }
 
 func getHost(r *http.Request) string {
-    host := r.Host
+	if i := strings.Index(r.Host, ":"); i != -1 {
+		return r.Host[:i]
+	}
 
-    if i := strings.Index(r.Host, ":"); i != -1 {
-        host = r.Host[:i]
-    }
-
-    return host
+	return r.Host
 }
 
 func serveRawIP(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, r.RemoteAddr)
-}
-
-var (
-    indexTemplate = template.Must(template.ParseFiles("index.html"))
-)
-
-type templateParams struct {
-    IP string
+	fmt.Fprint(w, r.RemoteAddr)
 }
 
 func serveHTML(w http.ResponseWriter, r *http.Request) {
-    params := templateParams{IP: r.RemoteAddr}
+	indexTemplate := template.Must(template.ParseFiles("index.html"))
 
-    indexTemplate.Execute(w, params)
+	param := map[string]string{
+		"IP": r.RemoteAddr,
+	}
+
+	indexTemplate.Execute(w, param)
 }
