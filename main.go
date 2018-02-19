ab0d88b46e2a8a4754b0f46778c579ad2fb39136
Cygnan <email@cygnan.com>
2018-02-19 22:46:57 +0900

    Add HTTP headers (#12)
    * Add HTTP headers
    
    * Change addHeader into addHeaders
    
    * Use the for statement

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "https://check-ip.ml", 301)
 	}
 
+	w = addHeaders(w)
+
 	if strings.Contains(r.Host, "raw") {
 		serveRawIP(w, r)
 	} else {
@@ -36,6 +38,24 @@ func getHost(r *http.Request) string {
 	}
 
 	return r.Host
+}
+
+func addHeaders(w http.ResponseWriter) http.ResponseWriter {
+	headers := map[string]string{
+		"Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+		"X-Frame-Options":           "DENY",
+		"X-XSS-Protection":          "1; mode=block",
+		"X-Content-Type-Options":    "nosniff",
+		"Content-Security-Policy":   "default-src 'none'",
+		"Referrer-Policy":           "no-referrer",
+		"X-Robots-Tag":              "noarchive",
+	}
+
+	for k, v := range headers {
+		w.Header().Set(k, v)
+	}
+
+	return w
 }
 
 func serveRawIP(w http.ResponseWriter, r *http.Request) {
